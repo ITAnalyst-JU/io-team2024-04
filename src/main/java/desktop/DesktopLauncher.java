1719f0f8ffd37e1de5b23355f64aa2e13a6c2c21
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2024-07-25 20:03:17 +0200

    Added test suite

---
+++ src/main/java/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import core.Program;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("TheProphet");
+		new Lwjgl3Application(new Program(), config);
+	}
+}
---
+++ src/test/java/core/ProgramTest.java
@@ -0,0 +1,10 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+
+public class ProgramTest {
+    @Test
+    public void testMain() {
+        Program program = new Program();
+    }
+}
