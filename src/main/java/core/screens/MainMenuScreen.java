1e9e8916f1095d609b35da7a7cca42aca0218949
loordoza <131matmil131@gmail.com>
2024-07-29 13:44:24 +0200

    Add keyboard navigation and focus handling

--- src/main/java/core/screens/MainMenuScreen.java
+++ src/main/java/core/screens/MainMenuScreen.java
@@ -1,9 +1,11 @@
 package core.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import core.Program;
 
@@ -18,6 +21,9 @@ public class MainMenuScreen implements Screen {
     private final Program game;
     private Stage stage;
     private Skin skin;
+    private TextButton startButton;
+    private TextButton exitButton;
+
     public MainMenuScreen(final Program game) {
         this.game = game;
     }
@@ -34,12 +40,16 @@ public class MainMenuScreen implements Screen {
         textButtonStyle.font = font;
         skin.add("default", textButtonStyle);
 
+        TextButton.TextButtonStyle focusedTextButtonStyle = new TextButton.TextButtonStyle(textButtonStyle);
+        focusedTextButtonStyle.fontColor = Color.RED;
+        skin.add("focused", focusedTextButtonStyle);
+
         Label.LabelStyle labelStyle = new Label.LabelStyle();
         labelStyle.font = font;
         skin.add("default", labelStyle);
 
-        TextButton startButton = new TextButton("Start game", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
+        startButton = new TextButton("Start game", skin);
+        exitButton = new TextButton("Exit", skin);
 
         Table table = new Table();
         table.setFillParent(true);
@@ -53,23 +63,104 @@ public class MainMenuScreen implements Screen {
 
         stage.addActor(table);
 
+        startButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new LevelSelectScreen(game));
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
         startButton.addListener(new InputListener() {
             @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new LevelSelectScreen(game));
-                return true;
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                stage.setKeyboardFocus(startButton);
+                updateButtonFocus();
             }
         });
 
         exitButton.addListener(new InputListener() {
             @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.exit();
-                return true;
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                stage.setKeyboardFocus(exitButton);
+                updateButtonFocus();
+            }
+        });
+
+        stage.setKeyboardFocus(startButton);
+        updateButtonFocus();
+
+        stage.addListener(new InputListener() {
+            @Override
+            public boolean keyDown(InputEvent event, int keycode) {
+                if (stage.getKeyboardFocus() != null) {
+                    switch (keycode) {
+                        case com.badlogic.gdx.Input.Keys.UP:
+                            moveFocus(Direction.UP);
+                            return true;
+                        case com.badlogic.gdx.Input.Keys.DOWN:
+                            moveFocus(Direction.DOWN);
+                            return true;
+                        case com.badlogic.gdx.Input.Keys.ENTER:
+                        case com.badlogic.gdx.Input.Keys.SPACE:
+                            if (stage.getKeyboardFocus() instanceof TextButton focusedButton) {
+                                focusedButton.toggle();
+                                InputEvent clickEvent = new InputEvent();
+                                clickEvent.setType(InputEvent.Type.touchDown);
+                                focusedButton.fire(clickEvent);
+
+                                clickEvent.setType(InputEvent.Type.touchUp);
+                                focusedButton.fire(clickEvent);
+                            }
+                            return true;
+                    }
+                }
+                return false;
             }
         });
     }
 
+    private void moveFocus(Direction direction) {
+        Table table = (Table) stage.getActors().get(0);
+        TextButton currentFocus = (TextButton) stage.getKeyboardFocus();
+        int currentIndex = table.getChildren().indexOf(currentFocus, true);
+
+        if (direction == Direction.UP) {
+            if (currentIndex > 1) {
+                stage.setKeyboardFocus(table.getChildren().get(currentIndex - 1));
+            } else {
+                stage.setKeyboardFocus(table.getChildren().get(table.getChildren().size - 1));
+            }
+        } else if (direction == Direction.DOWN) {
+            if (currentIndex < table.getChildren().size - 1) {
+                stage.setKeyboardFocus(table.getChildren().get(currentIndex + 1));
+            } else {
+                stage.setKeyboardFocus(table.getChildren().get(1));
+            }
+        }
+        updateButtonFocus();
+    }
+
+    private void updateButtonFocus() {
+        if (stage.getKeyboardFocus() == startButton) {
+            startButton.setStyle(skin.get("focused", TextButton.TextButtonStyle.class));
+            exitButton.setStyle(skin.get("default", TextButton.TextButtonStyle.class));
+        } else if (stage.getKeyboardFocus() == exitButton) {
+            startButton.setStyle(skin.get("default", TextButton.TextButtonStyle.class));
+            exitButton.setStyle(skin.get("focused", TextButton.TextButtonStyle.class));
+        }
+    }
+
+    enum Direction {
+        UP,
+        DOWN
+    }
 
     @Override
     public void render(float delta) {
