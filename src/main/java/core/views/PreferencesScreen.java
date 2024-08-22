eab5a6e2905f2877d2c9e8ecb9e663da7608bdf5
loordoza <131matmil131@gmail.com>
2024-08-22 21:00:29 +0200

    Preferences basic version

--- src/main/java/core/views/PreferencesScreen.java
+++ src/main/java/core/views/PreferencesScreen.java
@@ -1,11 +1,33 @@
 package core.views;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import desktop.preferences.PreferencesOrchestrator;
 
 public class PreferencesScreen extends UIScreen {
 
-    public PreferencesScreen(Stage stage) {
+    public PreferencesScreen(Stage stage, PreferencesOrchestrator preferencesOrchestrator) {
         super(stage);
+
+        addSlider("Music Volume", 0, 100, 1, preferencesOrchestrator.getMusicVolume(), () -> {
+            float newVolume = getSliderValue(0);
+            preferencesOrchestrator.setMusicVolume(newVolume);
+        });
+
+        addCheckbox("Enable Music", preferencesOrchestrator.isMusicEnabled(), () -> {
+            boolean isEnabled = getCheckboxState(0);
+            preferencesOrchestrator.setMusicEnabled(isEnabled);
+        });
+
+        addSlider("Sound Effects Volume", 0, 100, 1, preferencesOrchestrator.getSoundVolume(), () -> {
+            float newVolume = getSliderValue(1);
+            preferencesOrchestrator.setSoundVolume(newVolume);
+        });
+
+        addCheckbox("Enable Sound Effects", preferencesOrchestrator.isSoundEffectsEnabled(), () -> {
+            boolean isEnabled = getCheckboxState(1);
+            preferencesOrchestrator.setSoundEffectsEnabled(isEnabled);
+        });
+
         addButton("Back to Menu", () -> notifyOrchestrator(ScreenEnum.MENU));
     }
 }
--- src/main/java/core/views/ScreenAbstractFactory.java
+++ src/main/java/core/views/ScreenAbstractFactory.java
@@ -3,12 +3,19 @@ package core.views;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import core.levels.AbstractLevel;
+import desktop.preferences.PreferencesOrchestrator;
 
 public class ScreenAbstractFactory {
+    PreferencesOrchestrator preferencesOrchestrator;
+
+    public ScreenAbstractFactory(PreferencesOrchestrator preferencesOrchestrator) {
+        this.preferencesOrchestrator = preferencesOrchestrator;
+    }
+
     public AbstractScreen createScreen(ScreenEnum screenEnum) {
         return switch (screenEnum) {
             case MENU -> new MenuScreen(new Stage(new ScreenViewport()));
-            case PREFERENCES -> new PreferencesScreen(new Stage(new ScreenViewport()));
+            case PREFERENCES -> new PreferencesScreen(new Stage(new ScreenViewport()), preferencesOrchestrator);
             case ENDGAME -> new EndScreen(new Stage(new ScreenViewport()));
             case LOADING -> new LoadingScreen(new Stage(new ScreenViewport()));
             case LEVELSELECTION -> new LevelSelectionScreen(new Stage(new ScreenViewport()));
