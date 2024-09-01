34e992f5fff70f01a32e495a28a0846fbce02841
Michał Bawołek <michal.bawolek@gmail.com>
2024-09-01 21:20:56 +0200

    Changed asset loading to manager

--- src/main/java/core/entities/EntityFactory.java
+++ src/main/java/core/entities/EntityFactory.java
@@ -8,21 +8,25 @@ import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import core.assets.IAssetManager;
+import core.assets.IAssetManagerGetter;
 import core.entities.decorators.*;
 import core.general.Constants;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class NewEntityFactory {
+public class EntityFactory {
     private final Vector2 baseEntitySize;
     private final World world;
+    private final IAssetManagerGetter assetManager;
 
     private final Map<Integer, IEntity> buttonsMap = new HashMap<>();
 
-    public NewEntityFactory(Vector2 baseEntitySize, World world) {
+    public EntityFactory(Vector2 baseEntitySize, World world, IAssetManagerGetter assetManager) {
         this.baseEntitySize = baseEntitySize;
         this.world = world;
+        this.assetManager = assetManager;
     }
 
     public Map<Integer, IEntity> getButtonsMap() {
@@ -84,7 +88,8 @@ public class NewEntityFactory {
         } else {
             throw new UnsupportedOperationException("Unknown object name: " + type);
         }
-        Texture texture = new Texture(texturePath); // TODO: use asset manager
+        Texture texture = new Texture(texturePath);
+//        Texture texture = assetManager.getTexture(texturePath); // TODO: this should work
         entity = new SpriteDecorator(entity, new Sprite(texture), size);
 
         MovingDecorator.MovementDirection movementDirection;
@@ -123,7 +128,8 @@ public class NewEntityFactory {
             throw new UnsupportedOperationException("We only produce from rectangles");
         }
         IEntity entity = new BaseEntity(world, baseEntitySize, ((RectangleMapObject)mapObject).getRectangle().getPosition(new Vector2()));
-        Texture texture = new Texture("player/player.png");
+//        Texture texture = new Texture("player/player.png");
+        Texture texture = assetManager.getTexture("player/player.png");
         entity = new SpriteDecorator(entity, new Sprite(texture), baseEntitySize);
 
         Player player = new Player(entity);
