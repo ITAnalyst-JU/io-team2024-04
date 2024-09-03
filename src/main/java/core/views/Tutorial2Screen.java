e2533ec3fa022a3a08f35f33ab551ead9bc68a77
loordoza <131matmil131@gmail.com>
2024-09-03 18:52:52 +0200

    Buttons fix

--- src/main/java/core/views/Tutorial2Screen.java
+++ src/main/java/core/views/Tutorial2Screen.java
@@ -1,29 +1,27 @@
 package core.views;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import core.assets.AssetManagerFactory;
 
 public class Tutorial2Screen extends UIScreen {
-    private Table highScoreTable;
 
     public Tutorial2Screen(Stage stage, AssetManagerFactory assetManagerFactory) {
         super(stage, assetManagerFactory);
         setBackgroundImage("ui/background/tutorial2.png");
 
-        Label titleLabel = createLabel("Tutorial");
-        table.top().padTop(50);
-        table.add(titleLabel).expandX().padBottom(50);
+        table.add(createLabelWithBackground("Tutorial")).padTop(50);
         table.row();
-        table.add(createButton("Previous", () -> notifyOrchestrator(ScreenEnum.TUTORIAL))).expandX().padTop(20);
-        table.row();
-        table.add(createButton("Main menu", () -> notifyOrchestrator(ScreenEnum.MENU))).expandX().padTop(20);
-        table.row();
-    }
 
-    @Override
-    public void show() {
-        super.show();
+        Table buttonTable = new Table();
+        buttonTable.top().right();
+        buttonTable.padTop(50).padRight(50);
+
+        var nextButton = createButton("Previous", () -> notifyOrchestrator(ScreenEnum.TUTORIAL));
+        var mainMenuButton = createButton("Main menu", () -> notifyOrchestrator(ScreenEnum.MENU));
+
+        buttonTable.add(nextButton).padRight(10);
+        buttonTable.add(mainMenuButton);
+        table.add(buttonTable).expand().fill();
     }
 }
--- src/main/java/core/views/TutorialScreen.java
+++ src/main/java/core/views/TutorialScreen.java
@@ -1,29 +1,27 @@
 package core.views;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import core.assets.AssetManagerFactory;
 
 public class TutorialScreen extends UIScreen {
-    private Table highScoreTable;
 
     public TutorialScreen(Stage stage, AssetManagerFactory assetManagerFactory) {
         super(stage, assetManagerFactory);
         setBackgroundImage("ui/background/tutorial.png");
 
-        Label titleLabel = createLabel("Tutorial");
-        table.top().padTop(50);
-        table.add(titleLabel).expandX().padBottom(50);
+        table.add(createLabelWithBackground("Tutorial")).padTop(50);
         table.row();
-        table.add(createButton("Next", () -> notifyOrchestrator(ScreenEnum.TUTORIAL2))).expandX().padTop(20);
-        table.row();
-        table.add(createButton("Main menu", () -> notifyOrchestrator(ScreenEnum.MENU))).expandX().padTop(20);
-        table.row();
-    }
 
-    @Override
-    public void show() {
-        super.show();
+        Table buttonTable = new Table();
+        buttonTable.top().right();
+        buttonTable.padTop(50).padRight(50);
+
+        var nextButton = createButton("Next", () -> notifyOrchestrator(ScreenEnum.TUTORIAL2));
+        var mainMenuButton = createButton("Main menu", () -> notifyOrchestrator(ScreenEnum.MENU));
+
+        buttonTable.add(nextButton).padRight(10);
+        buttonTable.add(mainMenuButton);
+        table.add(buttonTable).expand().fill();
     }
 }
